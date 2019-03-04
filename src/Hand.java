fefb49c8abc410d83e80bb3f7120a62e73f2a7a8
blake <blake@Titanium>
2019-03-03 16:30:15 -0800

    Improved hand display

--- src/Hand.java
+++ src/Hand.java
@@ -15,18 +15,26 @@ public class Hand extends Deck {
 	}
 	@Override
 	public void draw() {
+		rearrange();
+		
 		drawer.fill(255, 0, 255);
 		drawer.rect(x, y, DECK_WIDTH*10, DECK_HEIGHT);
+		for(int i = 0; i < deck.size(); i++) {
+			deck.get(i).draw();
+		}
+	}
+	
+	// sets position of cards. Please call every time amount/position of cards in hand changes.
+	public void rearrange() {
 		if (deck.size()>=10) {
 			float shift = 0;
 			for (int i = 0; i < deck.size(); i++) {
 				deck.get(i).setX(x + shift);
 				deck.get(i).setY(y);
-				deck.get(i).draw();
 				shift += (float) (10.0 * Deck.DECK_WIDTH / deck.size());
 			}
 		} else if(deck.size()>0) {
-			
+			/* old way of displaying cards
 			float shift = Deck.DECK_WIDTH*5 - Deck.DECK_WIDTH/2;
 			for (int i = 0; i < deck.size(); i++) {
 				deck.get(i).setX(x + shift);
@@ -35,6 +43,14 @@ public class Hand extends Deck {
 				
 				shift -= Deck.DECK_WIDTH/2;
 				
+			}
+			*/
+			
+			float offset = ((10-deck.size())*DECK_WIDTH)/2.0f;
+			for(int i = 0; i < deck.size(); i++) {
+				deck.get(i).setX(x + offset);
+				deck.get(i).setY(y);
+				offset += Deck.DECK_WIDTH;
 			}
 
 		}
